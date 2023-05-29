e5a62321b60f7b30614ff63a7a16f067b9d0a7f0
MrMelchior <shannon2000.sp@gmail.com>
2023-05-29 18:06:27 +0200

    1.19.1 mod update tutorial optimization

--- src/main/java/net/mrmelchior/greaterspirits/GreaterSpirits.java
+++ src/main/java/net/mrmelchior/greaterspirits/GreaterSpirits.java
@@ -31,10 +31,8 @@ public class GreaterSpirits
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
-            
+            ModMessages.register();
         });
-
-        ModMessages.register();
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
